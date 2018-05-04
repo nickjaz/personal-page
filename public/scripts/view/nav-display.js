8267a1a7fd25d02004ec1bd13c63ab6ae6f83f33
Nick Asmussen <nick.asmussen1@gmail.com>
2018-05-03 17:39:13 -0700

    adds assets and cross to header

--- public/scripts/view/nav-display.js
+++ public/scripts/view/nav-display.js
@@ -5,15 +5,21 @@ $('.nav-icon').on('click', function() {
   var $mainNav = $('.main-nav');
   var $navContent = $('.nav-content');
   var $navList = $('.main-nav ul li');
+  var $navCross = $('.icon-cross');
+  var $navHamburger = $('.icon-menu');
 
   $navIcon.on('click', function() {
     $navIcon.toggleClass('active');
     $mainNav.toggleClass('active');
     $navContent.toggleClass('hidden');
+    $navCross.toggleClass('active');
+    $navHamburger.toggleClass('hidden');
   });
 
   $navList.on('click', function() {
     $mainNav.removeClass('active');
     $navContent.removeClass('hidden');
+    $navHamburger.removeClass('hidden');
+    $navCross.removeClass('active');
   });
 });
